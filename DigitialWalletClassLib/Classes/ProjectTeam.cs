0df086237c237811e30d37305dc5639634fa7568
Myles-Glenn Gaerlan <m.gaerlan@live.com>
2018-06-04 12:06:29 +1000

    Added skeleton code to class library

---
+++ DigitialWalletClassLib/Classes/ProjectTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitialWalletClassLib
+{
+    class ProjectTeam
+    {
+        private string _teamID { get; set; }
+        private string _teamName { get; set; }
+        private string _accountNo { get; set; }
+    }
+}
---
+++ DigitialWalletClassLib/Classes/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitialWalletClassLib
+{
+    class User
+    {
+        private string _accountNo { get; set; }
+        private string _firstName { get; set; }
+        private string _lastName { get; set; }
+        private string _phoneNo { get; set; }
+        private string _userID { get; set; }
+        private string _userType { get; set; }
+    }
+}
